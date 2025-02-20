12a89c975decace5bfd3e4b79dbe806e0c64d0b6
David DeGraw <degraw@fastmail.com>
2025-02-19 21:28:27 -0700

    updates

--- Casks/jazz-jackrabbit-2.rb
+++ Casks/jazz-jackrabbit-2.rb
@@ -1,15 +1,15 @@
 cask "jazz-jackrabbit-2" do
   version :latest
-  sha256 "ac48826f3d93c6241840e8dcf1e776072afc942b96b266ed83f58a417607ce40"
+  sha256 "beede38928b28c2423ef8b78c6010f3bef065035a13b81155b7af2a510fa8bc4"
 
-  url "https://github.com/deathkiller/jazz2/releases/download/3.1.0/Jazz2_3.1.0_MacOS.zip"
+  url "https://github.com/deathkiller/jazz2/releases/download/3.2.0/Jazz2_3.2.0_MacOS.zip"
   name "Jazz Jackrabbit 2"
   desc <<~DESC
-Although never as big as Mario or Sonic, Jazz Jackrabbit did well enough with a hungry PC gaming crowd to merit a second game in the series. It's your standard shareware sequel story: more levels, more twists, better technology. If you liked the original you'll like this one.It also has a very confusing release strategy. The original game was shareware, when you bought it you got the full Jazz Jackrabbit 2 game. Later, it was re-released with an additional episode under the title Jazz Jackrabbit 2: The Secret Files. Then came a release called Jazz Jackrabbit 2: The Christmas Chronicles, which adds Christmas-themed levels. So when you get the game on GOG you might spot two entries, neither of which look like they're the base game, but both should work in Jazz² Resurrection.
+    Although never as big as Mario or Sonic, Jazz Jackrabbit did well enough with a hungry PC gaming crowd to merit a second game in the series. It's your standard shareware sequel story: more levels, more twists, better technology. If you liked the original you'll like this one.It also has a very confusing release strategy. The original game was shareware, when you bought it you got the full Jazz Jackrabbit 2 game. Later, it was re-released with an additional episode under the title Jazz Jackrabbit 2: The Secret Files. Then came a release called Jazz Jackrabbit 2: The Christmas Chronicles, which adds Christmas-themed levels. So when you get the game on GOG you might spot two entries, neither of which look like they're the base game, but both should work in Jazz² Resurrection.
 Developer:  Epic MegaGames
 Release Date:  May 7, 1998
-  DESC
-  homepage "https://macsourceports.com/game/jazzjackrabbit2"
+DESC
+homepage "https://macsourceports.com/gamepage"
 
   app "Jazz² Resurrection.app"
 
--- Casks/rollercoaster-tycoon-2.rb
+++ Casks/rollercoaster-tycoon-2.rb
@@ -1,15 +1,15 @@
-cask "rollercoaster-tycoon-" do
+cask "rollercoaster-tycoon-2" do
   version :latest
   sha256 "3ae6d17eef586ff88d16e81e93ff2ce7c3300a9d89873526dcbbdb57ab24d6d1"
 
   url "https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.4.18/OpenRCT2-v0.4.18-macos-universal.zip"
   name "RollerCoaster Tycoon 2"
   desc <<~DESC
-Another game from the mind of Chris Sawyer, RollerCoaster Tycoon 2 shares the same pixel art style and hardcore interface as his other games.
+    Another game from the mind of Chris Sawyer, RollerCoaster Tycoon 2 shares the same pixel art style and hardcore interface as his other games.
 Developer:  Chris Sawyer
 Release Date:  October 15, 2002
-  DESC
-  homepage "https://macsourceports.com/game/rct2"
+DESC
+homepage "https://macsourceports.com/gamepage"
 
   app "OpenRCT2.app"
 
