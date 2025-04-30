dfcb2e15846695ec9f3e66fde962aa67d7501aea
David DeGraw <degraw@fastmail.com>
2025-04-29 23:16:05 -0600

    update dune 2000

--- Casks/dune-2000.rb
+++ Casks/dune-2000.rb
@@ -1,15 +1,15 @@
 cask "dune-2000" do
   version :latest
-  sha256 "da81dfcfab8287f21127827e029a0553bde08207a15656393c9e3c1d6341d3c4"
+  sha256 "2a78cd58603fd06ed6006ae5916065455a7ac7c1290dba1d06c0292bad4238ab"
 
-  url "https://github.com/OpenRA/OpenRA/releases/download/release-20231010/OpenRA-release-20231010.dmg"
+  url "https://github.com/OpenRA/OpenRA/releases/download/release-20250330/OpenRA-release-20250330.dmg"
   name "Dune 2000"
   desc <<~DESC
-A partial remake of Dune II, itself considered the earliest example of the mold that later RTS games would follow, Dune 2000 was initially released for PCs in 1998 and the PlayStation in 1999.Dune 2000 is not currently available via any digital distribution sales channels but OpenRA can download a subset of data to get going. Installing the full game's data from a disc is also supported.
+    A partial remake of Dune II, itself considered the earliest example of the mold that later RTS games would follow, Dune 2000 was initially released for PCs in 1998 and the PlayStation in 1999.Dune 2000 is not currently available via any digital distribution sales channels but OpenRA can download a subset of data to get going. Installing the full game's data from a disc is also supported.
 Developer:  Intelligent Games
 Release Date:  September 4, 1998
-  DESC
-  homepage "https://macsourceports.com/game/dune2000"
+DESC
+homepage "https://macsourceports.com/game/dune2000"
 
   app "OpenRA - Dune 2000.app"
 
